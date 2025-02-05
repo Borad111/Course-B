6734e72b08a3f67adb99e7f9cb9d7b51b77a6235
Borad111 <meetpatel39501010@gmail.com>
2025-02-05 20:22:57 +0530

    Update CoursePurchase.Controller.js

--- Controllers/CoursePurchase.Controller.js
+++ Controllers/CoursePurchase.Controller.js
@@ -43,8 +43,8 @@ const CreateCheackOutSession = async (req, res) => {
         },
       ],
       mode: "payment",
-      success_url: `http://localhost:5173/course-progress/${courseId}`, // once payment successful redirect to course progress page
-      cancel_url: `http://localhost:5173/course-detail/${courseId}`,
+      success_url: `https://backendc-1-qq4i.onrender.com/course-progress/${courseId}`, // once payment successful redirect to course progress page
+      cancel_url: `https://backendc-1-qq4i.onrender.com/course-detail/${courseId}`,
       metadata: {
         courseId: courseId,
         userId: userId,
